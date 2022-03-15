f31b9c4dc3dad0cf0b80127252d12eb0bf0d5655
Bizotto <bizotto.nicolas@gmail.com>
2022-03-15 11:22:30 -0300

    primeira lista

--- Exercicios/src/components/produtos/Listaprodutos.js
+++ Exercicios/src/components/produtos/Listaprodutos.js
@@ -4,17 +4,20 @@ import estilo from '../estilo';
 import produtos from './produtos';
 
 export default props => {
+  function obterLista() {
+    return produtos.map(p => {
+      return (
+        <Text key={p.id}>
+          {p.id}
+          {p.nome} tem o valor de R$ {p.preco}
+        </Text>
+      );
+    });
+  }
   return (
     <>
-      <Text style={estilo.txtG}>lista de produtos</Text>
-
-      {produtos.map(p => {
-        return (
-          <Text>
-            {p.id} {p.nome}{' '}
-          </Text>
-        );
-      })}
+      <Text style={estilo.txtG}>Lista produtos</Text>
+      {obterLista()}
     </>
   );
 };
